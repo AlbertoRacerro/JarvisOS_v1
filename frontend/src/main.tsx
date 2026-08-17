68815a935f4cd7db742cad9d8019bfa726a990fe
AlbertoRacerro <alberto.racerro@mail.polimi.it>
2026-08-17 13:39:34 +0200

    Restore readable main entry formatting

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -16,4 +16,9 @@ import "./styles/settings.css";
 import "./styles/responsive.css";
 
 applyStoredAppearance();
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(<React.StrictMode><App /></React.StrictMode>);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
